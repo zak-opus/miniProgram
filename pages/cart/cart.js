e5d5c97764b239a0b622d99380619e0076fc1a29
zak-opus <akak7@foxmail.com>
2019-11-22 15:05:16 +0800

    app({})page({})注册小程序与注册页面知识

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,42 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('购物车页面被加载')
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('购物页面渲染完成')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('购物页面显示')
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('购物页面被隐藏')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+      console.log('购物车页面销毁')
   },
 
   /**
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,47 @@
 // pages/home/home.js
+// 注册一个页面
 Page({
+  // 1.初始化绑定数据
+  data:{
+    list:[]
+  },
+  // 3.监听wxml相关事件
   handleGetUserInfo(event){
     console.log(event)
+  },
+  // 2.监听生命周期
+  onLoad(){
+    console.log('页面被加载完成')
+    wx.request({
+      url: 'http://123.207.32.32:8000/home/multidata',
+      success: (res) => {
+        const data = res.data.data.keywords.list;
+        this.setData({
+          list: data
+        })
+      }
+    })
+  },
+  onShow(){
+    console.log('页面显示出来')
+  },
+  onReady(){
+    console.log('页面渲染完成')
+  },
+  onHide(){
+    console.log('页面关闭')
+  },
+  onUnload(){
+    console.log('页面销毁')
+  },
+  // 4.监听其他事件：页面滚动、上拉下拉加载更多
+  onPageScroll(obj){
+    // console.log(obj)
+  },
+  onReachBottom(){
+    console.log('滚动到底部')
+  },
+  onPullDownRefresh(){
+    console.log('下拉加载更多')
   }
 })
