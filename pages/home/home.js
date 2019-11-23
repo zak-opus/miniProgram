d48f43cead8d581a341f22701094209a5fa4e2c0
zak-opus <akak7@foxmail.com>
2019-11-23 13:48:51 +0800

    上传本地相册

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,25 @@
 // pages/home/home.js
 Page({
-  handleGetUserInfo(event){
-    console.log(event)
+  data:{
+    imagePath:'',
+    isTrue:true,
+    list:[
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ]
+  },
+  handleChooseAlbum:function(){
+    wx.chooseImage({
+      success: (res)=>{
+        const path = res.tempFilePaths[0]
+        this.setData({
+          imagePath:path
+        })
+      }
+    })
+  },
+  aa(){
+    console.log('加载了一张')
   }
 })
